18d7abb015f0356b5b475bb6a00a3db2cbb6a3ac
Nicole Agurcia <nicole_agurcia@unitec.edu>
2015-09-13 16:30:38 +0200

    Nave,Fondo,Balas
    Spaceman

---
+++ NaveG/NaveGame/Assets/Scripts/Bala.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bala : MonoBehaviour {
+	public float velocidad = 30f;
+	public float maxima_d = 12f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0,1,0)*velocidad*Time.deltaTime);
+
+		if(transform.position.z > maxima_d){
+			Destroy(gameObject);
+
+		}
+	
+	}
+
+	void Start(){
+		transform.Rotate (90, 0, 0);
+	}
+}
---
+++ NaveG/NaveGame/Assets/Scripts/Jugador.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jugador : MonoBehaviour {
+	public float velocidad  =10f;
+	public float maximo_x= 19f;
+	public float maximo_y= 10f;
+	public GameObject Lanzador_L;
+	public GameObject Lanzador_R;
+	public GameObject bala;
+
+	void Update () {
+		//Movimiento Horizontal
+		if (Input.GetAxis("Horizontal") != 0 && transform.position.x < maximo_x && transform.position.x > -	maximo_x) {
+			transform.Translate(new Vector3(1,0,0)*velocidad*Time.deltaTime*Input.GetAxis("Horizontal"));
+		}
+		if (transform.position.x >= maximo_x) {
+			transform.Translate(new Vector3(-maximo_x-0.01f,0,0)*Time.deltaTime);
+		}
+		if (transform.position.x <= -maximo_x) {
+			transform.Translate(new Vector3(maximo_x+0.01f,0,0)*Time.deltaTime);
+		}
+
+		//Movimiento Vertical
+		if (Input.GetAxis("Vertical") != 0 && transform.position.z < maximo_y && transform.position.z > -maximo_y) {
+			transform.Translate(new Vector3(0,0,1)*velocidad*Time.deltaTime*Input.GetAxis("Vertical"));
+		}
+		if (transform.position.z >= maximo_y) {
+			transform.Translate(new Vector3(0,0,-maximo_y-0.01f)*Time.deltaTime);
+		}
+		if (transform.position.z <= -maximo_y) {
+			transform.Translate(new Vector3(0,0,maximo_y+0.01f)*Time.deltaTime);
+		}
+
+		//Lanzamiento Proyectiles
+		if (Input.GetButtonDown ("Jump")) {
+			Instantiate(bala,new Vector3 (Lanzador_L.transform.position.x,Lanzador_L.transform.position.y,Lanzador_L.transform.position.z),transform.rotation);
+			Instantiate(bala,new Vector3 (Lanzador_R.transform.position.x,Lanzador_R.transform.position.y,Lanzador_R.transform.position.z),transform.rotation);
+
+		}
+
+	}
+}
