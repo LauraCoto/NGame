68847daf7e1d83eece72aca9736fa9e8ea8d1bc2
Nicole Agurcia <nicole_agurcia@unitec.edu>
2015-09-14 17:44:11 -0500

    Reparando arma, Generando enemigos
    Reparando error de la arma, ahora ya tenemos enemigos

---
+++ NaveG/NaveGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float velocidad = 30f;
+	public float maxima_d = 12f;
+
+	void Update () {
+		transform.Translate(new Vector3(0,0,-1)*velocidad*Time.deltaTime);	
+		if(transform.position.z < -maxima_d){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ NaveG/NaveGame/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+	//dificultad del enemigo
+	public float NPM = 60f;
+	public GameObject Enemigo;
+	public float rango = 19f;
+	public bool disponible =true;
+
+
+	void Update () {
+		if (disponible) {
+			StartCoroutine("Instantiate");
+			}
+	}
+
+	IEnumerator Instantiate (){
+		disponible = false;
+		Instantiate (Enemigo, new Vector3 (Random.Range (-rango, rango), 4, transform.position.z), transform.rotation);
+		yield return new WaitForSeconds (60f / NPM);
+		disponible = true;
+	}
+}
